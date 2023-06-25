bd622b537d25c1ad29b90966d0703dfb582ab190
Istiniel <eclipsio@mail.ru>
2023-06-25 21:26:00 +0300

    feat: os task done

---
+++ src/subtasks/os.js
@@ -0,0 +1,34 @@
+import { osCommands } from '../static/commands.js';
+import { arch } from 'node:process';
+import { cpus, EOL, userInfo, homedir } from 'node:os';
+
+export async function os(option) {
+  let result;
+
+  if (!option || !osCommands.includes(option)) {
+    throw new Error('Wrong os command option');
+  }
+
+  switch (option.slice(2)) {
+    case 'EOL':
+      result = JSON.stringify(EOL);
+      break;
+    case 'cpus':
+      result = cpus().map((core) => {
+        const { model, speed } = core;
+        return { model, speed: `${speed / 1000} GHz` };
+      });
+      break;
+    case 'homedir':
+      result = userInfo().homedir;
+      break;
+    case 'username':
+      result = userInfo().username;
+      break;
+    case 'architecture':
+      result = arch;
+      break;
+  }
+
+  console.log(result);
+}
